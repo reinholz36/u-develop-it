751d9663725c5d433aaf44a04240355d64e44caf
reinholz36 <dreinholz28@gmail.com>
2022-01-31 15:53:58 -0500

    added practice sql query statements

--- server.js
+++ server.js
@@ -26,6 +26,34 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+//     // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     // console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
 // Default response for any other request (Not Found) Always place at end of list for app.use as it will overide 
 app.use((req, res) => {
     res.status(404).end();
